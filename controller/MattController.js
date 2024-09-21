18d338b855b456944236356e2376edb52376ea1d
Glen Victa <135594386+gmvicta@users.noreply.github.com>
2024-09-21 11:53:57 +0800

    Add files via upload

--- controller/MattController.js
+++ controller/MattController.js
@@ -2,10 +2,12 @@ const User = require('../models/User');
 
 const users = [];
 
+// METHOD #1
 exports.main = (req, res) => {
     res.render('index', { users });
 };
 
+// METHOD #2
 exports.addUser = (req, res) => {
     const { name, email } = req.body;
     const newUser = new User(name, email);
@@ -13,16 +15,19 @@ exports.addUser = (req, res) => {
     res.redirect('/');
 };
 
+// METHOD #3
 exports.getUsers = (req, res) => {
     res.render('users', { users });
 };
 
+// METHOD #4
 exports.deleteUser = (req, res) => {
     const index = req.params.index;
     users.splice(index, 1);
     res.redirect('/users');
 };
 
+// METHOD #5
 exports.updateUser = (req, res) => {
     const index = req.params.index;
     const { name, email } = req.body;
