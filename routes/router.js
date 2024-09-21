fff465acd86ebb6b63b6293ac0c5bb42f26e0b04
Glen Victa <135594386+gmvicta@users.noreply.github.com>
2024-09-21 11:54:26 +0800

    Add files via upload

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,10 @@ const express = require("express");
 const glen = express.Router();
 const matt = require("../controller/MattController");
 
-glen.get('/', matt.main);
-glen.post('/add-user', matt.addUser);
-glen.get('/users', matt.getUsers);
-glen.post('/update-user/:index', matt.updateUser);
-glen.post('/delete-user/:index', matt.deleteUser);
+glen.get('/', matt.main); // #1
+glen.post('/add-user', matt.addUser); // #2
+glen.get('/users', matt.getUsers); // #3
+glen.post('/update-user/:index', matt.updateUser); // #4
+glen.post('/delete-user/:index', matt.deleteUser); // #5
 
 module.exports = glen;
